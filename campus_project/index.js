f5d2f2e26fac2e5be77c7e6bbd7987eaeac469c9
ricky <59864164+ricky603@users.noreply.github.com>
2021-01-17 15:32:24 +0700

    Add files via upload

--- campus_project/index.js
+++ campus_project/index.js
@@ -1,8 +1,16 @@
+function checkData() {
+    if ($('tbody').children().length < 1) {
+        $('tbody').append('<tr id="no-data"><td colspan="10">No Data</td></tr>')
+    } else {
+        $('#no-data').remove()
+    }
+}
+
 $(document).ready(function () {
     $('#student-data').submit(function (e) { 
         e.preventDefault();
-        
     });
+    checkData()
 });
 
 function insertStudent() {
@@ -15,6 +23,29 @@ function insertStudent() {
     var uts = $('[name=uts_score]').val()
     var uas = $('[name=uas_score]').val()
 
-    var studentValueRow = `<tr scope="row"><td>${nim}</td><td>${name}</td><td>${prodi}</td><td>${semester}</td><td>${quiz}</td><td>${tugas}</td><td>${uts}</td><td>${uas}</td></tr>`
+    var ip;
+    var na = (0.10*quiz) + (0.20*tugas) + (0.30*uts) + (0.40*uas)
+
+    if ((na > 80) && (na <=100)) {
+        ip = "A (Lulus Dengan Sangat Baik)"
+    } else if ((na > 68) && (na <=80)) {
+        ip = "B (Lulus Dengan Baik)"
+    } else if((na > 55) && (na <= 68)) {
+        ip = "C (Lulus Dengan Cukup)"
+    } else  if((na > 38) && (na <=55)) {
+        ip = "D (Lulus dengan Kurang)"
+    } else {
+        ip = "E (Tidak Lulus)"
+    }
+
+    var deleteButton = '<button class="btn btn-primary" onclick="deleteRow(this)" type="button">Hapus</button>'
+    var studentValueRow = `<tr scope="row"><td>${nim}</td><td>${name}</td><td>${prodi}</td><td>${semester}</td><td>${quiz}</td><td>${tugas}</td><td>${uts}</td><td>${uas}</td><td>${ip}</td><td>${deleteButton}</td></tr>`
+    document.getElementById("student-data").reset()
     $('tbody').append(studentValueRow)
+    checkData()
+}
+
+function deleteRow(el) {
+    $(el).parent().parent().remove()
+    checkData()
 }
