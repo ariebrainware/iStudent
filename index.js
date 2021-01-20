8b399e4b63febe7cedcffacb7f751d9122e78684
Arie Brainware <publicu001@protonmail.com>
2021-01-20 22:49:22 +0700

    Implement local storage

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+var deleteButton = '<button class="btn btn-primary" onclick="deleteRow(this)" type="button">Hapus</button>'
+
 function checkData() {
+    const s = JSON.parse(localStorage.getItem("student_data"))
+    console.log(s.name)
+    var studentValueRow = `<tr scope="row"><td>${s.nim}</td><td>${s.name}</td><td>${s.prodi}</td><td>${s.semester}</td><td>${s.quiz}</td><td>${s.tugas}</td><td>${s.uts}</td><td>${s.uas}</td><td>${s.ip}</td><td>${deleteButton}</td></tr>`
+    document.getElementById("student-data").reset()
+    $('tbody').append(studentValueRow)
     if ($('tbody').children().length < 1) {
         $('tbody').append('<tr id="no-data"><td colspan="10">No Data</td></tr>')
-    } else {
+    } else {        
         $('#no-data').remove()
     }
 }
 
 $(document).ready(function () {
-    $('#student-data').submit(function (e) { 
+    $('#student-data').submit(function (e) {
         e.preventDefault();
     });
     checkData()
@@ -24,21 +31,25 @@ function insertStudent() {
     var uas = $('[name=uas_score]').val()
 
     var ip;
-    var na = (0.10*quiz) + (0.20*tugas) + (0.30*uts) + (0.40*uas)
+    var na = (0.10 * quiz) + (0.20 * tugas) + (0.30 * uts) + (0.40 * uas)
 
-    if ((na > 80) && (na <=100)) {
+    if ((na > 80) && (na <= 100)) {
         ip = "A (Lulus Dengan Sangat Baik)"
-    } else if ((na > 68) && (na <=80)) {
+    } else if ((na > 68) && (na <= 80)) {
         ip = "B (Lulus Dengan Baik)"
-    } else if((na > 55) && (na <= 68)) {
+    } else if ((na > 55) && (na <= 68)) {
         ip = "C (Lulus Dengan Cukup)"
-    } else  if((na > 38) && (na <=55)) {
+    } else if ((na > 38) && (na <= 55)) {
         ip = "D (Lulus dengan Kurang)"
     } else {
         ip = "E (Tidak Lulus)"
     }
 
-    var deleteButton = '<button class="btn btn-primary" onclick="deleteRow(this)" type="button">Hapus</button>'
+    const studentData = {
+        name,nim,prodi,semester,quiz,tugas,uts,uas,ip
+    }
+    localStorage.setItem("student_data",JSON.stringify(studentData))
+    
     var studentValueRow = `<tr scope="row"><td>${nim}</td><td>${name}</td><td>${prodi}</td><td>${semester}</td><td>${quiz}</td><td>${tugas}</td><td>${uts}</td><td>${uas}</td><td>${ip}</td><td>${deleteButton}</td></tr>`
     document.getElementById("student-data").reset()
     $('tbody').append(studentValueRow)
