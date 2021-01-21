92578d619ca488daea0a43570a9025bece85c671
Arie Brainware <publicu001@protonmail.com>
2021-01-21 21:28:15 +0700

    Refactor code

--- index.js
+++ index.js
@@ -1,38 +1,79 @@
-var deleteButton = '<button class="btn btn-primary" onclick="deleteRow(this)" type="button">Hapus</button>'
+// Global State Data
+let s = []
 
-function checkData() {
-    const s = JSON.parse(localStorage.getItem("student_data"))
-    console.log(s.name)
-    var studentValueRow = `<tr scope="row"><td>${s.nim}</td><td>${s.name}</td><td>${s.prodi}</td><td>${s.semester}</td><td>${s.quiz}</td><td>${s.tugas}</td><td>${s.uts}</td><td>${s.uas}</td><td>${s.ip}</td><td>${deleteButton}</td></tr>`
-    document.getElementById("student-data").reset()
-    $('tbody').append(studentValueRow)
-    if ($('tbody').children().length < 1) {
-        $('tbody').append('<tr id="no-data"><td colspan="10">No Data</td></tr>')
-    } else {        
-        $('#no-data').remove()
+// Form Field Variable
+const studentName = $('#name')
+const nim = $('#nim')
+const prodi = $('#prodi')
+const semester = $('#semester')
+const quizScore = $('#quiz_score')
+const tugasScore = $('#tugas_score')
+const utsScore = $('#uts_score')
+const uasScore = $('#uas_score')
+
+
+// Component/DOM Variable
+const btnAdd = $('#btn-add')
+const modalAdd = $('#modal-add')
+const formStudentData = $('#form-student-data')
+const tableStudentData = $('tbody')
+const btnClose = $('#btn-close')
+const btnSimpan = $('#btn-simpan')
+// const btnDeleteField = $('#btn-delete-field')
+const noDataInfo = () => {
+    return `<tr id="no-data"><td colspan="10">No Data</td></tr>`
+}
+const btnDelete = nim => {
+    return `<button id="btn-delete-${nim}" class="btn btn-primary" onClick="deleteStudentScore(${nim})" type="button">Hapus</button>`
+}
+
+/*
+#####################################################################
+Function Definition
+*/
+const successMessage = () => {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Student score data saved',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
+
+const clearField = () => {
+    studentName[0].value = ''
+    nim[0].value = ''
+    prodi[0].value = ''
+    semester[0].value = ''
+    quizScore[0].value = ''
+    tugasScore[0].value = ''
+    utsScore[0].value = ''
+    uasScore[0].value = ''
+}
+
+const loadData = () => {
+    const data = JSON.parse(localStorage.getItem("student_data"))
+    s = data
+    if (data) {
+        data.map(studentRow => {
+            tableStudentData.append(createStudentRow(studentRow))
+        })
+    } else {
+        noDataInfo()
     }
 }
 
-$(document).ready(function () {
-    $('#student-data').submit(function (e) {
-        e.preventDefault();
-    });
-    checkData()
-});
+const createStudentRow = data => {
+    console.log('createStudentRowData', data)
+    return `<tr scope="row"><td>${data.nim}</td><td>${data.name}</td><td>${data.prodi}</td><td>${data.semester}</td><td>${data.quiz_score}</td><td>${data.tugas_score}</td><td>${data.uts_score}</td><td>${data.uas_score}</td><td>${data.ip}</td><td><button id="btn-delete" class="btn btn-primary" onClick="deleteStudentScore(${data.nim})" type="button">Hapus</button></td></tr>`
+}
 
-function insertStudent() {
-    var name = $('[name=name]').val()
-    var nim = $('[name=NIM]').val()
-    var prodi = $('[name=prodi]').val()
-    var semester = $('[name=semester]').val()
-    var quiz = $('[name=quiz_score]').val()
-    var tugas = $('[name=tugas_score]').val()
-    var uts = $('[name=uts_score]').val()
-    var uas = $('[name=uas_score]').val()
+const insertStudentScore = () => {
+    let ip
+    let na = (0.10 * quizScore[0].value) + (0.20 * tugasScore[0].value) + (0.30 * utsScore[0].value) + (0.40 * uasScore[0].value)
 
-    var ip;
-    var na = (0.10 * quiz) + (0.20 * tugas) + (0.30 * uts) + (0.40 * uas)
-
+    // Determine Alphabet Score
     if ((na > 80) && (na <= 100)) {
         ip = "A (Lulus Dengan Sangat Baik)"
     } else if ((na > 68) && (na <= 80)) {
@@ -46,17 +87,47 @@ function insertStudent() {
     }
 
     const studentData = {
-        name,nim,prodi,semester,quiz,tugas,uts,uas,ip
+        "name": studentName[0].value,
+        "nim": nim[0].value,
+        "prodi": prodi[0].value,
+        "semester": semester[0].value,
+        "quiz_score": quizScore[0].value,
+        "tugas_score": tugasScore[0].value,
+        "uts_score": utsScore[0].value,
+        "uas_score": uasScore[0].value,
+        "ip": ip,
     }
-    localStorage.setItem("student_data",JSON.stringify(studentData))
-    
-    var studentValueRow = `<tr scope="row"><td>${nim}</td><td>${name}</td><td>${prodi}</td><td>${semester}</td><td>${quiz}</td><td>${tugas}</td><td>${uts}</td><td>${uas}</td><td>${ip}</td><td>${deleteButton}</td></tr>`
-    document.getElementById("student-data").reset()
-    $('tbody').append(studentValueRow)
-    checkData()
+    if (s.length > 0) {
+        s.push(studentData)
+        localStorage.setItem('student_data', JSON.stringify(s))
+    }
+    tableStudentData.append(createStudentRow(studentData))
+    successMessage()
+    clearField()
 }
 
-function deleteRow(el) {
-    $(el).parent().parent().remove()
-    checkData()
+const deleteStudentScore = (nim) => {
+    const data = JSON.parse(localStorage.getItem("student_data"))
+    if (data) {
+        data.filter(value => {
+            if (value.nim != nim) {
+                console.log('not deleted NIM: ', value.nim)
+                localStorage.setItem('student_data', JSON.stringify(data))
+            }
+        })
+    }
 }
+
+/*
+###########################################################
+Event Listener
+*/
+window.addEventListener('load', () => {
+    loadData()
+
+    formStudentData.on('submit', (event) => {
+        event.preventDefault()
+        insertStudentScore()
+        formStudentData[0].setAttribute("data-dismiss", "modal")
+    })
+})
